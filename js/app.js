975bc916964f8c919aaf0d826dcb229300023aba
unknown <tomygarnero9429@gmail.com>
2022-11-18 14:31:34 -0300

    Added prototype msj validation

--- js/app.js
+++ js/app.js
@@ -22,6 +22,27 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+// Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement("div");
+    if(tipo === "error") {
+        div.classList.add("error");
+    } else {
+        div.classList.add("correcto");
+    }
+
+    div.classList.add("mensaje", "mt-10");
+    div.textContent = mensaje;
+
+    // Insertar en el HTML
+    const formulario = document.querySelector("#cotizar-seguro");
+    formulario.insertBefore(div, document.querySelector("#resultado"));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000)
+}
+
 // Instanciar IU
 const ui = new UI();
 
@@ -50,8 +71,14 @@ function cotizarSeguro(e) {
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
     if(marca === "" || year === "" || tipo === "") {
-        console.log("No paso la validacion")
-    } else {
-        console.log("Si paso la validacion")
+        ui.mostrarMensaje("Todos los campos son obligatorios", "error");
+        return;
     }
+
+    ui.mostrarMensaje("Cotizando...", "exito");
+
+    // Instanciar el seguro
+
+    // Utilzar el prototype que va a cotizar
+    
 }
