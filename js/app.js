aee1d9e6eb5f5c00dc1de23ead9a38fd5a7bc811
unknown <tomygarnero9429@gmail.com>
2022-11-18 13:45:56 -0300

    Added form validation

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// Constructores
+function Seguro(marca, yaer, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo
+}
+
+function UI() {}
+
+// Llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear();
+    const min = max - 20;
+
+    const selectYear = document.querySelector("#year");
+
+    for(let i = max; i > min; i--) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
+
+// Instanciar IU
+const ui = new UI();
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    ui.llenarOpciones();  // lenna el select con los años
+})
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector("#cotizar-seguro");
+    formulario.addEventListener("submit", cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector("#marca").value;
+
+    // Leer el año seleccionado
+    const year = document.querySelector("#year").value;
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if(marca === "" || year === "" || tipo === "") {
+        console.log("No paso la validacion")
+    } else {
+        console.log("Si paso la validacion")
+    }
+}
